8ac5dd23b0074a56c2b30b63f58c33cec36e8eed
safiawarsame <gcab022031@coderacademy.edu.au>
2021-01-20 10:51:53 +1000

    main.rb

--- safiawarsame_T1A3/src/main.rb
+++ safiawarsame_T1A3/src/main.rb
@@ -15,13 +15,14 @@ end
 if !name1
  puts "please, enter the first player's name".blue
  name1=STDIN.gets.strip 
- 
+end
+if!name2
  puts "please, enter the second player's name".blue
- name2=gets.strip 
+ name2=STDIN.gets.strip 
 
 answer="replay"
 while(answer!="Exit")
-     t = TicTacToe.new(name1,name2,players_no)
+     t = TicTacToe.new(name1,name2)
      t.play
     end
 answer=$prompt.select("Do you want to play another game?",["replay","Exit"])
