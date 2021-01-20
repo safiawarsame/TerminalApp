e10dd12ff9ff03f1182c474201294491745e97f0
safiawarsame <gcab022031@coderacademy.edu.au>
2021-01-20 10:49:23 +1000

    main.rb

--- safiawarsame_T1A3/src/main.rb
+++ safiawarsame_T1A3/src/main.rb
@@ -12,25 +12,15 @@ if ARGV.length > 0
 elsif ARGV.length ==2
     name2 = ARGV[1]   
 end 
-
+if !name1
+ puts "please, enter the first player's name".blue
+ name1=STDIN.gets.strip 
+ 
+ puts "please, enter the second player's name".blue
+ name2=gets.strip 
 
 answer="replay"
 while(answer!="Exit")
-    answer=$prompt.select("Choose an options?",["1 player","2 players","Exit"])
-    if answer == "1 player"
-      players_no=1
-      puts "please, enter your name".blue   
-      name1=gets.strip
-      t = TicTacToe.new(name1,"ai",players_no)
-      t.play
-    elsif answer == "2 players"  
-     players_no=2  
-     puts "please, enter the first player's name".blue
-     name1=gets.strip 
-     puts "please, enter the second player's name".blue
-     name2=gets.strip 
-     
-
      t = TicTacToe.new(name1,name2,players_no)
      t.play
     end
